c1d7551053a8883127a46fe8116902dee82aff91
YiMa <yima9102@gmail.com>
2014-02-10 01:51:05 -0500

    parse.py only

--- data_parse/parse.py
+++ data_parse/parse.py
@@ -4,13 +4,15 @@ import sys,os
 symbols = {"%hes":"","%spk":"","%int":"","noise":""}
 addition = {"--":" "}
 # how to use those 
-def parseTxtFile (fileName, dirName):
+
+
+def parse_txt_file (fileName, dirName):
 	file1 = open(fileName)
 	file2 = open(dirName + "cesar_data.txt.text","w+")
 	file3 = open(dirName + "cesar_data.txt.feat","w+")
 	for line in file1:
 		text = line.split("\t")[-1]
-		#feat = line.split("\t")[-2]
+		feat = line.split("\t")[-2]
 		# only extract the text part
 		pattern = re.compile("[a-zA-Z\s\^'%()\-'<>]+")
 		text = re.search(pattern, text)
@@ -25,16 +27,35 @@ def parseTxtFile (fileName, dirName):
 				text = text.replace(s,symbols.get(s))
 				# delete multiple spaces
 
-				text = text.replace("\s{2,}", " ")
-				text = re.sub(format, "",text)
-				if text:
-					file2.write(text+"\n")
-					# if feat=="driver":
-					# 	bi_feat = 1;
-					# else:
-					# 	if feat == "copilot":
-					# 		bi_feat = 0;
-					# file3.write(str(bi_feat)+"\n")
+			text = text.replace("\s{2,}", " ")
+			text = re.sub(format, "",text)
+			if text:
+				file2.write(text+"\n")
+				if feat=="driver":
+					 bi_feat = 1;
+				else:
+					if feat == "copilot":
+						bi_feat = 0;
+				file3.write(str(bi_feat)+"\n")
+
+def generate_vocabulary(fileName, dirName):
+	textFile = open(fileName)
+	vocab = open("vocab_" + dirName + ".txt","w+")
+	vocabulary = set([])
+
+	for line in textFile:
+		pattern = re.compile("[%<>\-/]z|\n")
+		nline = re.sub(pattern, "", line)
+		nline = nline.replace("\s{2,}"," ")
+		words = set(nline.split(" "))
+		vocabulary  = vocabulary | words
+
+		v = list(vocabulary)
+		v.sort()
+		for word in v:
+			if word:
+				vocab.write(word+"\n")
+
 
 
 dir = "../CESAR_data"
@@ -45,7 +66,8 @@ for root, dirs, files in os.walk(dir):
 		for r, d, files in os.walk(path):
 			if fileName in files:
 				print fileName
-				parseTxtFile(os.path.join(path,fileName), subDir)
+				parse_txt_file(os.path.join(path,fileName), subDir)
+				generate_vocabulary(subDir + "cesar_data.txt.text", subDir)
 
 
 
